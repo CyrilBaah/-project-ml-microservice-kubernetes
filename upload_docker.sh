73e65a405186dd82b26a7f4d4b25b5c6ffa064e7
CyrilBaah <cyril.baah@amalitech.org>
2022-07-28 11:39:01 +0000

    upload image to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=cyrilbaah/project-ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker tag app cyrilbaah/project-ml-microservice-kubernetes
+
 # Step 3:
 # Push image to a docker repository
+docker push cyrilbaah/project-ml-microservice-kubernetes:latest
