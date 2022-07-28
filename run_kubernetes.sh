a744b1a6d277ad9e5623b2209e28b3b4f6ff7c9d
CyrilBaah <cyril.baah@amalitech.org>
2022-07-28 12:39:17 +0000

    manifest files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=cyrilbaah/project-ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f k8_ml.yml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/ml-microservice 8000:80
 
