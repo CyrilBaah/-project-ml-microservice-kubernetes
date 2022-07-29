b09c00389c37345a5f5a32e313c80c328325675c
CyrilBaah <cyril.baah@amalitech.org>
2022-07-29 07:42:56 +0000

    prediction out | docker_out.txt

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     LOG.info(f"Prediction: \n{prediction}")
+    print("prediction made.")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
